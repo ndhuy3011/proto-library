7d7bff0b4b8ff3bf543b59fa21b70b7574211a62
duchuy <ndhuy30111@gmail.com>
2025-01-13 15:41:02 +0700

    init lib

---
+++ src/main/java/com/ndhuy/proto_library/ProtoLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.ndhuy.proto_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ndhuy/proto_library/ProtoLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndhuy.proto_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
